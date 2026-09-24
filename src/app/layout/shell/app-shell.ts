790661eec669a2d0748f6097505a5c08def9bf88
Daniele Dal Prà <110026310+ddalpra@users.noreply.github.com>
2026-09-24 22:47:07 +0200

    Sistemazione del progetto di Base

--- src/app/layout/shell/app-shell.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-shell',
-    templateUrl: './app-shell.html',
-    styleUrl: './app-shell.scss'
-})
-export class AppShell {}
